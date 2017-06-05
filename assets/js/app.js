e9b7469549a55b2837227cfffefcd4c302014d4a
Jonathan Grim <jonjongrim@gmail.com>
2017-06-05 17:18:55 -0400

    Adds play and pause icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
   // DOM cache
-  const searchText = $('#searchText');
-  const searchBtn = $('#searchBtn');
-  const giphyRow = $('#giphyRow');
+  const $searchText = $('#searchText');
+  const $searchBtn = $('#searchBtn');
+  const $giphyRow = $('#giphyRow');
+  const $play = $('#play');
+  const $pause = $('#pause');
 
   // Bind listeners
-  searchBtn.click(addGifs);
+  $searchBtn.click(addGifs);
   $('img').click(changeState);
 
+  // hide glyphicons to start
+  $play.fadeOut(0);
+  $pause.fadeOut(0);
+
   function addGifs() {
-    let searchTerms = searchText.val();
+    let searchTerms = $searchText.val();
     giphy.getThatGif(searchTerms, displayGifs);
   }
 
   function displayGifs(giphyArray) {
     let giphyChunks = _.chunk(giphyArray, 4);
-    // debugger;
-    giphyRow.prepend(
+
+    $giphyRow.prepend(
       giphyChunks.map(chunk => {
         let $gifRow = $('<div></div>').addClass('row');
         $gifRow.prepend(createGifRow(chunk));
@@ -59,22 +65,33 @@ $(document).ready(function() {
 
   function gifCaption(gif) {
     // let captionBits = [];
-    let $rating = $('<h5></h5>').text(`Rating: ${gif.rating}`);
-    let $url = $('<h5></h5>').text(`URL: ${gif.url}`);
-    let $source = $('<h5></h5>').text(`Source: ${gif.source}`);
+    let $rating = $('<h5></h5>').text(`Rating: ${gif.rating.toUpperCase()}`);
+    // let $url = $('<h5></h5>').text(`URL: ${gif.url}`);
+    // let $source = $('<h5></h5>').text(`Source: ${gif.source}`);
 
-    let $caption = $('<div></div>').append([$rating, $url, $source]);
+    let $caption = $('<div></div>').append([$rating]);
     return $caption;
   }
 
   function changeState(e) {
     let $gif = $(e.target);
+    let gifCoords = $gif[0].getBoundingClientRect();
     if ($gif.attr('state') === 'animate') {
       $gif.attr('src', $gif.attr('data-still')).attr('state', 'still');
       $gif.next('.caption').slideToggle();
+
+      $pause.css('height', gifCoords.height);
+      $pause.css('transform', `translate(${gifCoords.left + window.scrollX}px, ${gifCoords.top + window.scrollY}px)`);
+      $pause.fadeIn(200);
+      $pause.fadeOut('slow');
     } else {
       $gif.attr('src', $gif.attr('data-animate')).attr('state', 'animate');
       $gif.next('.caption').slideToggle();
+
+      $play.css('height', gifCoords.height);
+      $play.css('transform', `translate(${gifCoords.left + window.scrollX}px, ${gifCoords.top + window.scrollY}px)`);
+      $play.fadeIn(200);
+      $play.fadeOut('slow');
     }
   }
 });
